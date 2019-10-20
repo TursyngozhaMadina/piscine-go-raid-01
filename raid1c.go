4e5ef6d992f437df46200d427a9272fa6ee0f70b
TursyngozhaMadina <tmb.group.kz@gmail.com>
2019-10-20 15:32:51 +0600

    raid1c2

--- raid1c.go
+++ raid1c.go
@@ -1,27 +1,29 @@
-package student
+package main
 
 import "github.com/01-edu/z01"
 
-func Raid1c(x, y int) {
-
-	for i := 0; i < y; i++ {
-		for j := 0; j < x; j++ {
-			if (i == 0 && j == 0) || (i == 0 && j == x-1) {
-				z01.PrintRune('A')
-			} else if (i == y-1 && j == x-1) || (i == y-1 && j == 0) {
+func Raid1c(x,y int) {
+	for i := 1 ; i <= y ; i ++ {
+		for j := 1 ; j <= x ; j ++ {
+			if (i == 1 && j == 1) || (j == x && i == 1 ) {
+				z01.PrintRune('A') 
+			} else if (i == y && j == 1) || (j == x && i == y ) {
 				z01.PrintRune('C')
-			} else if (i == 0 || i == y-1) || (j == 0 || j == x-1) {
+			} else if i == 1 || i == y {
 				z01.PrintRune('B')
-			} else {
-				z01.PrintRune(' ')
+			} else if j == 1  || j == x {
+				z01.PrintRune('B')
+			}   else { 
+				z01.PrintRune(' ')		
+			} 
+			if j == x {
+				z01.PrintRune('\n')
 			}
+			
 		}
-		z01.PrintRune('\n')
+		
 	}
 }
 func main() {
-	student.Raid1c(5,3)
-	student.Raid1c(5,1)
-	student.Raid1c(1,1)
-	student.Raid1c(1,5)
+	Raid1c(1,5)
 }
